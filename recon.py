ceacf8f7bc6d70bf39a1d391b12149dbe7acaef6
Akhil Thampy <68418451+akhilwrist@users.noreply.github.com>
2022-01-02 20:53:19 +0530

    Update recon.py

--- recon.py
+++ recon.py
@@ -1,5 +1,13 @@
+print(r"""   ___  _        __  _____  __  __    __  ___   ___    __ 
+  / _ \/_\    /\ \ \/__   \/__\/__\  /__\/ __\ /___\/\ \ \
+ / /_)//_\\  /  \/ /  / /\/_\ / \// /_\ / /   //  //  \/ /
+/ ___/  _  \/ /\  /  / / //__/ _  \//__/ /___/ \_// /\  / 
+\/   \_/ \_/\_\ \/   \/  \__/\/ \_/\__/\____/\___/\_\ \/  
+             
+             https://github.com/akhilwrist                                               """)
+
 import os
-a=(input("enter the domain url :- "))
+a=(input("Enter the domain  :- "))
 cmd = 'echo '+a+'|subfinder | httpx |nuclei -t nuclei-templates'
 c=os.popen(cmd)
 d=c.read()
