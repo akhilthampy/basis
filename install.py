054bb5baa505cbee3e9a051370b046b6e2506985
Akhil Thampy <68418451+akhilwrist@users.noreply.github.com>
2022-01-02 20:55:22 +0530

    Update install.py

--- install.py
+++ install.py
@@ -1,3 +1,10 @@
+print(r"""                     
+ ._   __/__  /._  _ 
+// /_\ / /_|/// //_/
+                 _/  
+             
+             https://github.com/akhilwrist                                               """)
+
 import os
 cmd = 'go install -v github.com/projectdiscovery/subfinder/v2/cmd/subfinder@latest&&go install -v github.com/projectdiscovery/httpx/cmd/httpx@latest&&go install -v github.com/projectdiscovery/nuclei/v2/cmd/nuclei@latest'
 c=os.popen(cmd)
