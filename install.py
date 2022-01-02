ef286f988169f0f554852577b96bed9d64a3d797
Akhil Thampy <68418451+akhilthampy@users.noreply.github.com>
2022-01-02 12:26:05 +0530

    Create install.py

---
+++ install.py
@@ -0,0 +1,5 @@
+import os
+cmd = 'go install -v github.com/projectdiscovery/subfinder/v2/cmd/subfinder@latest&&go install -v github.com/projectdiscovery/httpx/cmd/httpx@latest&&go install -v github.com/projectdiscovery/nuclei/v2/cmd/nuclei@latest'
+c=os.popen(cmd)
+d=c.read()
+print(d)
